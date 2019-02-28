48af070981093b7239ab443e01e12ec5799e25ec
Jack Steam <jacksteamdev@gmail.com>
2019-02-28 10:41:49 -0600

    debug copy again button

--- src/notify.js
+++ src/notify.js
@@ -1,21 +1,12 @@
 import { notify } from '@bumble/chrome'
-import { listenTo, error } from '@bumble/stream'
+import { listenTo, error, log } from '@bumble/stream'
 import { writeText } from './writeText'
 
 const btnTitle = 'copy again'
 
-const {
-  name,
-  icons = {},
-  browser_action: { default_icon = {} } = {},
-} = chrome.runtime.getManifest()
+const { name, icons = {} } = chrome.runtime.getManifest()
 
-const iconUrl =
-  icons['128'] ||
-  icons['16'] ||
-  default_icon['16'] ||
-  default_icon['24'] ||
-  default_icon['32']
+const iconUrl = icons['128'] || icons['16']
 
 export const notifyCopy = txt =>
   writeText(txt)
@@ -31,7 +22,8 @@ export const notifyCopy = txt =>
 
 const handleBtnClick = txt => noteId => {
   listenTo(chrome.notifications.onButtonClicked)
-    .filter(id => id === noteId)
+    .forEach(log('onButtonClicked'))
+    .filter(({ id }) => id === noteId)
     .map(() => writeText(txt)) // have it copy to clipboard again
     .clear(() => true) //remove callback
     .catch(error('handleBtnClick'))
