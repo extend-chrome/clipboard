0cb934aead99bb91849852dc6a6dd92f3e600540
Jack Steam <jacksteamdev@gmail.com>
2019-02-28 17:14:35 -0600

    Refactor notifyCopy

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import { writeText } from './writeText'
 export { notifyCopy } from './notify'
 
 export const clipboard = { readText, writeText }
+
+/**
+ * @example
+ * clipboard.writeText(txt).then(notifyCopy)
+ *
+ * @example
+ * clipboard.readText().then((text) => {console.log('text in clipboard', text)})
+ */
--- src/notify.js
+++ src/notify.js
@@ -2,23 +2,27 @@ import { notify } from '@bumble/chrome'
 import { listenTo, error, log } from '@bumble/stream'
 import { writeText } from './writeText'
 
-const btnTitle = 'copy again'
+export const notifyCopy = txt => {
+  const btnTitle = 'copy again'
+  const { name, icons = {} } = chrome.runtime.getManifest()
+  const iconUrl =
+    icons[
+      Object.keys(icons)
+        .map(s => parseInt(s))
+        .reduce((r, x) => (r > x ? r : x), 0)
+        .toString()
+    ]
 
-const { name, icons = {} } = chrome.runtime.getManifest()
-
-const iconUrl = icons['128'] || icons['16']
-
-export const notifyCopy = txt =>
-  writeText(txt)
-    .then(() => ({
+  return notify
+    .create({
       type: 'basic',
       title: name,
       message: `"${txt}" was copied to the clipboard.`,
       iconUrl,
       buttons: [{ title: btnTitle }],
-    }))
-    .then(notify.create)
+    })
     .then(handleBtnClick(txt))
+}
 
 const handleBtnClick = txt => noteId => {
   listenTo(chrome.notifications.onButtonClicked)
